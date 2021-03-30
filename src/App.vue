ea92458336583271ded274e80c1114bea4e9ce75
sabika <skhan179@c02dr2zdmd6r.myfiosgateway.com>
2021-03-30 12:54:14 -0400

    Adding styling

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div> 
+        <h1>Friends List</h1>
+        <ul>
+            <li v-for="i in friends" :key="i">
+                {{i}}
+            </li>
+        </ul>
+
+    </div> 
+</template>
+
+<script>
+export default { 
+    data() { 
+    return { 
+        friends:["Manny", "Sabika", "Sam"]   
+    }; 
+    } 
+}
+</script>
+
+<style>
+    h1 {color:gray}
+    body {color:coral; background-color:black;border: 2px solid coral}
+</style>
